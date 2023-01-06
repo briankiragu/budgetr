7895a61c879d3c10eb13e5c3c13a05e15205f08b
Brian Kariuki <bkariuki@hotmail.com>
2023-01-06 13:47:46 +0300

    Finished installing TailwindCSS
    close #1

--- src/app.tsx
+++ src/app.tsx
@@ -1,7 +1,9 @@
-const App = () => {
+import type { Component } from 'solid-js';
+
+const App: Component = () => {
   return (
     <div>
-      <h1>BudgetR</h1>
+      <h1 class='text-3xl font-bold underline'>BudgetR</h1>
     </div>
   );
 };

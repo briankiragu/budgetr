204fdb25d3dba071eddfee04d68efdad9bc5f86f
Brian Kiragu <bkiragu@amazon.com>
2023-03-23 20:15:48 +0200

    Added config

--- src/composables/useIdentity.ts
+++
@@ -1,6 +0,0 @@
-export default () => {
-  const generateUid = (salt = 'ch'): string =>
-    `${salt}${Math.random().toString(36).slice(2, 9)}`;
-
-  return { generateUid };
-};
--- src/interfaces/user.ts
+++
@@ -1,8 +0,0 @@
-/* eslint-disable @typescript-eslint/naming-convention */
-
-import type { IBudget } from '@interfaces/budget';
-
-export type IUser = {
-  username: string;
-  budget: IBudget;
-};
